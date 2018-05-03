018914a5bbd771b937b0c3bebc5549dee35a4f79
Genteure <genteure@gmail.com>
2018-05-04 06:03:11 +0800

    Add models

---
+++ desktop/Bililive_dm_VR/Bililive_dm_VR.Desktop/Model/ConfigFile.cs
@@ -0,0 +1,45 @@
+﻿using BinarySerialization;
+using System.Collections.Generic;
+
+namespace Bililive_dm_VR.Desktop.Model
+{
+    public class ConfigFile
+    {
+        /// <summary>
+        /// 配置文件版本号
+        /// </summary>
+        [FieldOrder(0)]
+        public uint Version { get; set; } = 1;
+
+        /// <summary>
+        /// 上次连接的直播房间号
+        /// </summary>
+        [FieldOrder(1)]
+        [SerializeWhen(nameof(Version), 1)]
+        public int RoomId { get; set; } = 0;
+
+        /// <summary>
+        /// 配置数量
+        /// </summary>
+        [FieldOrder(2)]
+        [SerializeWhen(nameof(Version), 1)]
+        public int ProfileCount { get; set; } = 0;
+
+        /// <summary>
+        /// 配置列表
+        /// </summary>
+        [FieldOrder(3)]
+        [SerializeUntil((byte)0)]
+        [SerializeWhen(nameof(Version), 1)]
+        [ItemLength(nameof(ProfileCount))]
+        [FieldChecksum(nameof(Checksum), Mode = ChecksumMode.Xor)]
+        public List<Profile> Profiles { get; set; } = null;
+
+        /// <summary>
+        /// 校验位 
+        /// </summary>
+        [FieldOrder(4)]
+        [SerializeWhen(nameof(Version), 1)]
+        public byte Checksum { get; set; }
+    }
+}
---
+++ desktop/Bililive_dm_VR/Bililive_dm_VR.Desktop/Model/Profile.cs
@@ -0,0 +1,88 @@
+﻿using BinarySerialization;
+
+namespace Bililive_dm_VR.Desktop.Model
+{
+    public class Profile
+    {
+        /// <summary>
+        /// 配置文件名
+        /// </summary>
+        [FieldLength(0)]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 固定设备
+        /// </summary>
+        [FieldLength(1)]
+        public MountDevice MountDevice { get; set; }
+
+        /// <summary>
+        /// 固定位置
+        /// </summary>
+        [FieldLength(2)]
+        public MountLocation MountLocation { get; set; }
+
+        /// <summary>
+        /// 偏移坐标X
+        /// </summary>
+        [FieldLength(3)]
+        public int OffsetX { get; set; }
+
+        /// <summary>
+        /// 偏移坐标Y
+        /// </summary>
+        [FieldLength(4)]
+        public int OffsetY { get; set; }
+
+        /// <summary>
+        /// 偏移坐标Z
+        /// </summary>
+        [FieldLength(5)]
+        public int OffsetZ { get; set; }
+
+        /// <summary>
+        /// 透明度 ( 0 - 100 )
+        /// </summary>
+        [FieldLength(6)]
+        public int Alpha { get; set; }
+
+        /// <summary>
+        /// 缩放倍数 ( 100% )
+        /// </summary>
+        [FieldLength(7)]
+        public int Scale { get; set; }
+
+        /// <summary>
+        /// 颜色 ( AARRGGBB )
+        /// </summary>
+        [FieldLength(8)]
+        public int Color { get; set; }
+
+        /// <summary>
+        /// 动作类型
+        /// </summary>
+        [FieldLength(9)]
+        public AnimationType AnimationType { get; set; }
+
+        /// <summary>
+        /// 动作后透明度
+        /// </summary>
+        [FieldLength(10)]
+        public int AnimationAlpha { get; set; }
+
+        /// <summary>
+        /// 动作后缩放倍数
+        /// </summary>
+        [FieldLength(11)]
+        public int AnimationScale { get; set; }
+
+        /// <summary>
+        /// 显示的消息类型
+        /// </summary>
+        [FieldLength(12)]
+        public MessageType MessageType { get; set; }
+
+
+
+    }
+}
