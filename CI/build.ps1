fbf4b6374b6b942bef58bf455b82515a5a9f18fe
Genteure <genteure@gmail.com>
2018-06-08 08:23:41 +0800

    fix windows build

--- CI/build.ps1
+++ CI/build.ps1
@@ -1,9 +1,45 @@
+$ErrorActionPreference = "Stop"
+
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip {
+    param([string]$zipfile, [string]$outpath)
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+
 $basepath = Get-Location
 Set-Location .\desktop\Bililive_dm_VR\
 
+$builderror = $false
 nuget restore -verbosity quiet
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) {
+    $host.SetShouldExit($LastExitCode) 
+    $builderror = $true
+}
 msbuild /v:m /p:Configuration=Release /l:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
-if ($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode) }
+if ($LastExitCode -ne 0) {
+    $host.SetShouldExit($LastExitCode) 
+    $builderror = $true
+}
 
 Set-Location $basepath
+if ($builderror) {
+    Exit
+}
+
+$resultpath = './build/'
+$zipfilename = 'rendererbuild.zip'
+$wpfbuildpath = '.\desktop\Bililive_dm_VR\Bililive_dm_VR.Desktop\bin\Release\'
+
+$proj = Invoke-RestMethod -Method Get -Uri "https://ci.appveyor.com/api/projects/genteure/bililive-dm-vr/build/$env:APPVEYOR_BUILD_VERSION"
+Invoke-RestMethod -Method Get -Uri "https://ci.appveyor.com/api/buildjobs/$($proj.build.jobs[0].jobId)/artifacts/build.zip" -OutFile $zipfilename
+if (!(Test-Path $zipfilename)) {
+    throw "Artifact Download Failed"
+}
+Unzip $zipfilename $resultpath
+
+@("Bililive_dm_VR.Desktop.exe", "BinarySerializer.dll", "Newtonsoft.Json.dll", "Xceed.Wpf.Toolkit.dll").ForEach( 
+    {
+        Copy-Item "$($wpfbuildpath)$($_)" $resultpath
+    }
+)
+
