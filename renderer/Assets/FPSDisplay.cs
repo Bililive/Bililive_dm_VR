0be96a2dc48f3750ac42f56e336dd8a3b0345b0d
Genteure <genteure@gmail.com>
2018-06-09 08:07:01 +0800

    Changed screen size to 500x500 & move fps counter to right upper corner

--- renderer/Assets/FPSDisplay.cs
+++ renderer/Assets/FPSDisplay.cs
@@ -19,7 +19,7 @@ public class FPSDisplay : MonoBehaviour
         int w = Screen.width, h = Screen.height;
         GUIStyle style = new GUIStyle();
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
-        style.alignment = TextAnchor.UpperLeft;
+        style.alignment = TextAnchor.UpperRight;
         style.fontSize = h * 2 / 100;
         style.normal.textColor = new Color(0.0f, 0.0f, 0.5f, 1.0f);
         float msec = deltaTime * 1000.0f;
